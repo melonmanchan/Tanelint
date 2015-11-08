915d4573a8f7c10c08163b8abac176811e2de3b0
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-08 12:43:26 +0200

    Delete unused test directory

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import tanelint from '../lib';
-
-describe('tanelint', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
